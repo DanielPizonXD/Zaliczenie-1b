44860f5b7e1c558cb2e788f9ec79b3d8711fda9a
DanielPizonXD <126687080+DanielPizonXD@users.noreply.github.com>
2023-05-24 12:50:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main { 
+public static void main(String[] args) {
+
+String str = "kajak";
+String odwroconyStr = "";
+
+
+
+for (int i = str.length() - 1; i >=0; --i) {
+  odwroconyStr = odwroconyStr + str.charAt(i);
+}
+
+if (str.toLowerCase().equals(odwroconyStr.toLowerCase())) {
+  System.out.println(str + " .");
+}
+else {
+  System.out.println(str + " nie jest palindromem");
+}
+} 
+}
